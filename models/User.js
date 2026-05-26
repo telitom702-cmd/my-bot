e51e81f9fd322facd45906944471fdd3ab07b234
telitom702-cmd <telitom702@gmail.com>
2026-05-27 01:34:07 +0600

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,144 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  telegramId: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    default: ''
+  },
+  languageCode: {
+    type: String,
+    default: 'en'
+  },
+  isBot: {
+    type: Boolean,
+    default: false
+  },
+  balance: {
+    type: Number,
+    default: 0
+  },
+  totalEarnings: {
+    type: Number,
+    default: 0
+  },
+  dailyTasksCompleted: {
+    type: Number,
+    default: 0,
+    expires: '1d' // Daily reset
+  },
+  level: {
+    type: Number,
+    default: 1
+  },
+  xp: {
+    type: Number,
+    default: 0
+  },
+  xpToNextLevel: {
+    type: Number,
+    default: 100
+  },
+  referredBy: {
+    type: Number,
+    ref: 'User'
+  },
+  referrals: [{
+    type: Number,
+    ref: 'User'
+  }],
+  lastActive: {
+    type: Date,
+    default: Date.now
+  },
+  joinedAt: {
+    type: Date,
+    default: Date.now
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  isBanned: {
+    type: Boolean,
+    default: false
+  },
+  banReason: {
+    type: String
+  },
+  banDate: {
+    type: Date
+  },
+  ipAddress: {
+    type: String
+  },
+  deviceInfo: {
+    type: String
+  },
+  suspiciousActivityCount: {
+    type: Number,
+    default: 0
+  },
+  lastSuspiciousActivity: {
+    type: Date
+  }
+}, {
+  timestamps: true
+});
+
+// Virtual for full name
+UserSchema.virtual('fullName').get(function() {
+  return `${this.firstName} ${this.lastName}`.trim();
+});
+
+// Method to check if user can complete more tasks
+UserSchema.methods.canCompleteTask = function() {
+  return this.dailyTasksCompleted < process.env.MAX_DAILY_TASKS || 20;
+};
+
+// Method to add balance
+UserSchema.methods.addBalance = function(amount) {
+  this.balance += amount;
+  this.totalEarnings += amount;
+  return this.save();
+};
+
+// Method to check level up
+UserSchema.methods.checkLevelUp = function() {
+  if (this.xp >= this.xpToNextLevel) {
+    this.level += 1;
+    this.xp = this.xp - this.xpToNextLevel;
+    this.xpToNextLevel = Math.floor(this.xpToNextLevel * 1.5);
+    return true;
+  }
+  return false;
+};
+
+// Method to add XP
+UserSchema.methods.addXP = function(amount) {
+  this.xp += amount;
+  return this.checkLevelUp();
+};
+
+// Method to add referral
+UserSchema.methods.addReferral = function(refereeId) {
+  if (!this.referrals.includes(refereeId)) {
+    this.referrals.push(refereeId);
+    return this.save();
+  }
+  return Promise.resolve();
+};
+
+module.exports = mongoose.model('User', UserSchema);
